c0ab3c9d7291028599d27ed5e848163642f61e1f
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-18 16:16:37 -0600

    fix env variables

--- nestlebot_2020.py
+++ nestlebot_2020.py
@@ -8,10 +8,10 @@ from os import environ
 def main():
     
     # get twitter auth creds
-    CONSUMER_KEY = environ["TWITTER_CONSUMER_KEY"]
-    CONSUMER_SECRET = environ["TWITTER_CONSUMER_SECRET"]
-    ACCESS_TOKEN = environ["TWITTER_ACCESS_TOKEN"]
-    ACCESS_TOKEN_SECRET = environ["TWITTER_ACCESS_TOKEN_SECRET"]
+    CONSUMER_KEY = environ["CONSUMER_KEY"]
+    CONSUMER_SECRET = environ["CONSUMER_SECRET"]
+    ACCESS_TOKEN = environ["ACCESS_TOKEN"]
+    ACCESS_TOKEN_SECRET = environ["ACCESS_TOKEN_SECRET"]
     
     # set tweeting time interval
     #INTERVAL = 60 * 60 * 1  # tweet every 1 hour (prod)
