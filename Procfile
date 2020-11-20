abeab8cca1f3fc84beead4f49239f23302185856
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-20 12:21:19 -0600

    create files

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python nestlebot_2020.py
 worker: python nestlebot_2020.py
+worker: python text_detect.py
---
+++ text_detect.py
@@ -0,0 +1,6 @@
+# import packages
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import time
+import cv2
