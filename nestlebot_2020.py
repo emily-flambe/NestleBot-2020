559f4ab37c53b3ee855727b378e53ab2624085a5
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-20 11:15:34 -0600

    Update nestlebot_2020.py

--- nestlebot_2020.py
+++ nestlebot_2020.py
@@ -4,10 +4,10 @@ from os import environ
 import time
 import random
 import sys
-#from google_images_search import GoogleImagesSearch
-#from imutils.object_detection import non_max_suppression
+from google_images_search import GoogleImagesSearch
+from imutils.object_detection import non_max_suppression
 import cv2
-#import detect_text_function as dtf #lol
+import detect_text_function as dtf #lol
 
 def main():
     
