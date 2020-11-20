d569813bd0de1674234778b11a3e044c423e4780
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-20 12:44:10 -0600

    bug fix

--- detect_text_example.py
+++ detect_text_example.py
@@ -13,12 +13,12 @@ def main():
     #this is a photo of me with my favorite chicken. Her name is Spoopy. I love her. She is not text.
     spoopy_pic="images/spoopy.jpg"
     spoopy_result = dt.detect_text_from_image(spoopy_pic)
-    print(f"Does {spoopy_pic} contain text? {result}")
+    print(f"Does {spoopy_pic} contain text? {spoopy_result}")
     
     #this is a photo of a can of Carnation - a deity in the pantheon of horrible Nestle products. It has text.
     carnation_pic="images/nestle_carnation.jpg"
     carnation_result = dt.detect_text_from_image(carnation_pic)
-    print(f"Does {carnation_pic} contain text? {result}")
+    print(f"Does {carnation_pic} contain text? {carnation_result}")
 
 if __name__ == "__main__":
     main()
