aee96768424b12d9a0df837879e12ea33faacf38
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-24 10:00:31 -0600

    Update delete_all_tweets.py

--- delete_all_tweets.py
+++ delete_all_tweets.py
@@ -11,8 +11,35 @@ script, you can do so by registering a twitter application at https://dev.twitte
 @author: Dave Jeffery
 """
 
+from os import environ
 import tweepy
-from get_twitter_api import get_twitter_api
+
+def get_twitter_api():
+    
+    '''
+    Returns an API object to do Twittering
+    
+    Input: none BAYBEE
+    Output: returns API object
+    '''
+    # get twitter auth creds
+    CONSUMER_KEY = environ["TWITTER_CONSUMER_KEY"]
+    CONSUMER_SECRET = environ["TWITTER_CONSUMER_SECRET"]
+    ACCESS_TOKEN = environ["TWITTER_ACCESS_TOKEN"]
+    ACCESS_TOKEN_SECRET = environ["TWITTER_ACCESS_TOKEN_SECRET"]
+    
+    # set tweeting time interval
+    #INTERVAL = 60 * 60 * 1  # tweet every 1 hour (prod)
+    INTERVAL = 30  # every 15 seconds, for testing
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    
+    # Create API object
+    api = tweepy.API(auth)
+    
+    return api
 
 def batch_delete(api):
     print("You are about to Delete all tweets from the account @%s." % api.verify_credentials().screen_name)
